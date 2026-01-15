02b85b8883e50ead2a555b87a3b7aa7a172a9f91
ulgrude <73903561+ulgrude@users.noreply.github.com>
2026-01-15 18:48:12 +0100

    Remove the counter logic
    It was a feature that I removed

--- scripts.js
+++ scripts.js
@@ -11,43 +11,8 @@ function updateTime() {
 // Mise à jour de l'heure toutes les secondes
 setInterval(updateTime, 1000);
 
-// Fonction pour récupérer la date de la dernière remise à zéro
-function getLastResetDate() {
-    return localStorage.getItem('lastResetDate');
-}
-
-// Fonction pour sauvegarder la date de la dernière remise à zéro
-function saveLastResetDate(date) {
-    localStorage.setItem('lastResetDate', date);
-}
-
-// Fonction pour calculer le nombre de jours depuis la dernière remise à zéro
-function calculateDaysSinceLastReset() {
-    const lastResetDate = getLastResetDate();
-    if (lastResetDate) {
-        const lastDate = new Date(lastResetDate);
-        const today = new Date();
-        const timeDifference = today.getTime() - lastDate.getTime();
-        return Math.floor(timeDifference / (1000 * 3600 * 24));
-    } else {
-        console.log('Aucune date');
-        return 0;
-    }
-}
-
 // Initialisation de la page
 document.addEventListener('DOMContentLoaded', function() {
-    if (document.getElementById('daysCounter') != null) {
-        const daysSinceLastReset = calculateDaysSinceLastReset();
-        document.getElementById('daysCounter').textContent = daysSinceLastReset;
-
-        document.getElementById('resetButton').addEventListener('click', function() {
-            const today = new Date().toISOString().split('T')[0];
-            saveLastResetDate(today);
-            document.getElementById('daysCounter').textContent = '0';
-        });
-    }
-
     updateTime();
 });
 
@@ -55,4 +20,5 @@ window.addEventListener('load', function() {
     const randomImageElement = document.getElementById('randomImage');
     randomImageElement.src = 'random-image.php';
     randomImageElement.style.display = 'block';
+
 });
